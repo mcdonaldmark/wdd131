2586cdfe55d16f77b5f026de4d7ead3f6450a622
mcdonaldmark <markamcdonald7@gmail.com>
2025-03-17 11:50:52 -0600

    sync

--- scripts/place.js
+++ scripts/place.js
@@ -5,6 +5,3 @@ currentyear.innerHTML = `${today.getFullYear()}`;
 const modify = document.querySelector("#modified")
 const modified = (document.lastModified);
 modify.innerHTML = `${modified}`;
-
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
